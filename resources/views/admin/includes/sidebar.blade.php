42ad6c5b7e45f90c5bdcb312e5bcadd497313de0
our ame <me@karbainova.ru>
2024-05-23 20:46:58 +0300

    add tags

--- resources/views/admin/includes/sidebar.blade.php
+++ resources/views/admin/includes/sidebar.blade.php
@@ -4,12 +4,19 @@
     <!-- Sidebar -->
     <div class="sidebar">
         <ul class="pt-3 nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-
             <li class="nav-item">
                 <a href="{{ route('admin.category.index') }}" class="nav-link">
                     <i class="nav-icon fas fa-list-ul"></i>
                     <p>
                         Категории
+                    </p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="{{ route('admin.tag.index') }}" class="nav-link">
+                    <i class="nav-icon fas fa-tags"></i>
+                    <p>
+                        Теги
                     </p>
                 </a>
             </li>
